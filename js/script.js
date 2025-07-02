da89e5d2523f3c667b2781f729c7e63cb6263e19
anthony-murè <tony.mure99@gmail.com>
2025-07-02 16:11:22 +0200

    numeri multipli di (3 e 5) scritta FrizzBuzz al posto del numero

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
 
 for(let i = 1; i <=100; i++){
-  console.log(i)
+
+  if(i % 3 === 0 && i % 5 === 0){
+    console.log("FizzBuzz");
+  }
+  console.log(i);
 }
 
